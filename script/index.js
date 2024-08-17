ac0c01efa3832b3b7fac288fd46f07983effa3c8
romario <yohanesromario2311@gmail.com>
2024-08-17 22:10:03 +0700

    add script to simulate download file

--- script/index.js
+++ script/index.js
@@ -1,13 +1,38 @@
 const puppeteer = require('puppeteer');
-const fs = require('fs');
 const path = require('path');
 
-async function downloadFile() {
-    const browser = await puppeteer.launch()
-    const page = await browser.newPage()
-    await page.goto('https://example.com')
-    await page.screenshot({path: 'a.png'})
-    await browser.close()
+async function waitUntilDownload(page) {
+    return new Promise((resolve, reject) => {
+        page._client().on('Page.downloadProgress', e => { // or 'Browser.downloadProgress'
+            if (e.state === 'completed') {
+                resolve();
+            } else if (e.state === 'canceled') {
+                reject();
+            }
+        });
+    });
 }
 
-downloadFile();
+async function setDownloadBehaviour(page, downloadPath) {
+    await page._client().send('Page.setDownloadBehavior', {
+        behavior: 'allow',
+        downloadPath: downloadPath 
+    });
+}
+
+async function index() {
+    const downloadPath = path.resolve('./download');
+    const browser = await puppeteer.launch({
+        headless: true,
+    });
+    const page = await browser.newPage();
+    await page.goto(
+        'https://www.ksei.co.id/archive_download/holding_composition',
+        { waitUntil: 'networkidle2' }
+    );
+    await setDownloadBehaviour(page, downloadPath)
+    await page.click('.btn.btn--primary')
+    await waitUntilDownload(page)
+    await browser.close()
+}
+index();
